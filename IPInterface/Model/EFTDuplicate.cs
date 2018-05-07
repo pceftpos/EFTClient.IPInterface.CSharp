8813a09af4fe40d8b0415d8ed16ed4f5b8665bc3
Samuel Coianiz <scoianiz@pceftpos.com.au>
2018-05-08 08:30:01 +1000

    * Fixed a bug in EFTDuplicate that was constructing an EFTReceiptResponse with EFTRequest type EFTReceiptResponse instead of EFTReceiptRequest so it would throw an error and need to disconnect and reconnect to the client.

--- IPInterface/Model/EFTDuplicate.cs
+++ IPInterface/Model/EFTDuplicate.cs
@@ -1,18 +1,16 @@
-using System;
-
 namespace PCEFTPOS.EFTClient.IPInterface
 {
-    /// <summary>Reprint Type.</summary>
+	/// <summary>Reprint Type.</summary>
 	public enum ReprintType
-    {
-        /// <summary>Get the last receipt.</summary>
-        GetLast = '2',
-        /// <summary>Reprint the last receipt.</summary>
-        Reprint = '1'
-    }
+	{
+		/// <summary>Get the last receipt.</summary>
+		GetLast = '2',
+		/// <summary>Reprint the last receipt.</summary>
+		Reprint = '1'
+	}
 
 
-    /// <summary>A PC-EFTPOS duplicate receipt request object.</summary>
+	/// <summary>A PC-EFTPOS duplicate receipt request object.</summary>
 	public class EFTReprintReceiptRequest : EFTRequest
 	{
 		/// <summary>Constructs a default EFTDuplicateReceiptRequest object.</summary>
@@ -20,53 +18,53 @@ namespace PCEFTPOS.EFTClient.IPInterface
 		{
 		}
 
-        /// <summary>Two digit merchant code</summary>
-        /// <value>Type: <see cref="string"/><para>The default is "00"</para></value>
-        public string Merchant { get; set; } = "00";
+		/// <summary>Two digit merchant code</summary>
+		/// <value>Type: <see cref="string"/><para>The default is "00"</para></value>
+		public string Merchant { get; set; } = "00";
 
-        /// <summary>Indicates where the request is to be sent to. Should normally be EFTPOS.</summary>
-        /// <value>Type: <see cref="TerminalApplication"/><para>The default is <see cref="TerminalApplication.EFTPOS"/>.</para></value>
-        public TerminalApplication Application { get; set; } = TerminalApplication.EFTPOS;
+		/// <summary>Indicates where the request is to be sent to. Should normally be EFTPOS.</summary>
+		/// <value>Type: <see cref="TerminalApplication"/><para>The default is <see cref="TerminalApplication.EFTPOS"/>.</para></value>
+		public TerminalApplication Application { get; set; } = TerminalApplication.EFTPOS;
 
-        /// <summary>Indicates whether to trigger receipt events.</summary>
-        /// <value>Type: <see cref="ReceiptPrintModeType"/><para>The default is POSPrinter.</para></value>
-        public ReceiptPrintModeType ReceiptPrintMode { get; set; } = ReceiptPrintModeType.POSPrinter;
+		/// <summary>Indicates whether to trigger receipt events.</summary>
+		/// <value>Type: <see cref="ReceiptPrintModeType"/><para>The default is POSPrinter.</para></value>
+		public ReceiptPrintModeType ReceiptPrintMode { get; set; } = ReceiptPrintModeType.POSPrinter;
 
-        /// <summary>Indicates whether PC-EFTPOS should cut receipts.</summary>
-        /// <value>Type: <see cref="ReceiptCutModeType"/><para>The default is DontCut. This property only applies when <see cref="EFTRequest.ReceiptPrintMode"/> is set to EFTClientPrinter.</para></value>
-        public ReceiptCutModeType ReceiptCutMode { get; set; } = ReceiptCutModeType.DontCut;
+		/// <summary>Indicates whether PC-EFTPOS should cut receipts.</summary>
+		/// <value>Type: <see cref="ReceiptCutModeType"/><para>The default is DontCut. This property only applies when <see cref="EFTRequest.ReceiptPrintMode"/> is set to EFTClientPrinter.</para></value>
+		public ReceiptCutModeType ReceiptCutMode { get; set; } = ReceiptCutModeType.DontCut;
 
-        /// <summary>Indicates whether the receipt should be returned or reprinted.</summary>
-        /// <value>Type: <see cref="ReprintType"/><para>The default is GetLast.</para></value>
-        public ReprintType ReprintType { get; set; } = ReprintType.GetLast;
-    }
+		/// <summary>Indicates whether the receipt should be returned or reprinted.</summary>
+		/// <value>Type: <see cref="ReprintType"/><para>The default is GetLast.</para></value>
+		public ReprintType ReprintType { get; set; } = ReprintType.GetLast;
+	}
 
 	/// <summary>A PC-EFTPOS duplicate receipt response object.</summary>
 	public class EFTReprintReceiptResponse : EFTResponse
 	{
 		/// <summary>Constructs a default duplicate receipt response object.</summary>
-		public EFTReprintReceiptResponse() : base(typeof(EFTReprintReceiptResponse))
+		public EFTReprintReceiptResponse() : base(typeof(EFTReprintReceiptRequest))
 		{
 		}
 
-        /// <summary>Two digit merchant code</summary>
-        /// <value>Type: <see cref="string"/><para>The default is "00"</para></value>
-        public string Merchant { get; set; } = "00";
+		/// <summary>Two digit merchant code</summary>
+		/// <value>Type: <see cref="string"/><para>The default is "00"</para></value>
+		public string Merchant { get; set; } = "00";
 
-        /// <summary>Duplicate receipt text.</summary>
-        /// <value>Type: <see cref="System.String">String array</see></value>
-        public string[] ReceiptText { get; set; } = new string[] { "" };
+		/// <summary>Duplicate receipt text.</summary>
+		/// <value>Type: <see cref="System.String">String array</see></value>
+		public string[] ReceiptText { get; set; } = new string[] { "" };
 
-        /// <summary>Indicates if the request was successful.</summary>
-        /// <value>Type: <see cref="System.Boolean"/></value>
-        public bool Success { get; set; } = false;
+		/// <summary>Indicates if the request was successful.</summary>
+		/// <value>Type: <see cref="System.Boolean"/></value>
+		public bool Success { get; set; } = false;
 
-        /// <summary>The response code of the request.</summary>
-        /// <value>Type: <see cref="System.String"/><para>A 2 character response code. "00" indicates a successful response.</para></value>
-        public string ResponseCode { get; set; } = "";
+		/// <summary>The response code of the request.</summary>
+		/// <value>Type: <see cref="System.String"/><para>A 2 character response code. "00" indicates a successful response.</para></value>
+		public string ResponseCode { get; set; } = "";
 
-        /// <summary>The response text for the response code.</summary>
-        /// <value>Type: <see cref="System.String"/></value>
-        public string ResponseText { get; set; } = "";
-    }
+		/// <summary>The response text for the response code.</summary>
+		/// <value>Type: <see cref="System.String"/></value>
+		public string ResponseText { get; set; } = "";
+	}
 }
